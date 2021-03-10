96cf13de6609fa146bfd3b07990f6d927fe82327
PabloGri <74774797+PabloGri@users.noreply.github.com>
2021-03-10 14:01:48 +0100

    Update index.php
    Modification du test avec la table CACA qui contenait île flottante

--- index.php
+++ index.php
@@ -1,17 +1,24 @@
-<?php 
-
-require_once "functions.php";
-
-$bdd = get_Db();
-$requetes = $bdd->query('SELECT * FROM CACA');
-
-?>
-
-<body>
-    <?php 
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        
+        
+            require_once 'includes/functions.php';
+        
+            $bdd = getDb();
+        $requetes = $bdd->query('SELECT * FROM CACA');
         foreach($requetes as $requete)
         {
             echo $requete['GATEAU'];
         }
-    ?>
-</body>
+        
+        
+        
+        ?>
+    Lolo
+    </body>
+</html>
