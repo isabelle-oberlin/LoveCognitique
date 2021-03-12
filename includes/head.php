86e81814d613f2d044771378b24bc042509986d4
PabloGri <pgribal@ensc.fr>
2021-03-12 22:16:26 +0100

    Update head.php

--- includes/head.php
+++ includes/head.php
@@ -2,7 +2,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="../lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="../styling/css/style.css" rel="stylesheet">
-    <title>MyMovies <?php if (isset($pageTitle)) echo "- $pageTitle"; ?></title>
+    <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="styling/css/style.css" rel="stylesheet">
+    <title>LoveCo</title>
 </head>
