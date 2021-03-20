33eb96d1857c54ac2ba933d75dfd5080ab7e088e
PabloGri <pgribal@ensc.fr>
2021-03-20 20:27:06 +0100

    fixed inscription

--- inscription.php
+++ inscription.php
@@ -3,37 +3,8 @@
             include_once 'includes/head.php';
             include_once 'includes/functions.php';
             session_start();
-
-            //formulaire et création du compte. L'administrateur changera simplement le bool "validé"; 
-            //pour l'instant on peut créer un compte avec juste Nom, Promo, Mdp
-            if (!empty($_POST['Nom']) and !empty($_POST['Mot de passe']) and !empty($_POST['Promo']))
-            {
-                $bdd = getLocalDb();
-                //trouver l'id du nouveau compte
-                $query= $bdd->prepare('select max(IdAlumni) from Alumni');
-                $query->execute();
-                $new_id = $query->fetch();
-                //trouver l'id de la commune, pas accessible à l'élève qui s'inscrit
-                //fait changer le format. Demander le code postal par exemple
-                //$query= $bdd->prepare('select IdCommune from Commune where 'CodePostal = escape($_POST['AdressePostale'])');
-                //$idcommune=$query->fetch();
-
-                //FIXME le 42 à la place d'idcommune dans la requete
-                $stmt = $bdd->prepare('insert into Alumni (IdAlumni, NomEleve, PrenomEleve, Promo, AdressePostale, Mail, Mdp, Genre, Tel, Valide, IdCommune, IdGestionnaire) values (:IdAlumni, :NomEleve, :PrenomEleve, :Promo, :AdressePostale, :Mail, :Mdp, :Genre, :Tel, 0, 42, 0)');
-                $stmt->bindParam(':IdAlumni', $new_id);
-                $stmt->bindParam(':NomEleve', escape($_POST['IdAlumni']));
-                $stmt->bindParam(':PrenomEleve', escape($_POST['PrenomEleve']));
-                $stmt->bindParam(':Promo', escape($_POST['Promo']));
-                $stmt->bindParam(':AdressePostale', escape($_POST['AdressePostale']));
-                $stmt->bindParam(':Mail', escape($_POST['Mail']));
-                $stmt->bindParam(':Mdp', escape($_POST['Mdp']));
-                $stmt->bindParam(':Genre', escape($_POST['Genre']));
-                $stmt->bindParam(':Tel', escape($_POST['Tel']));
-                $stmt->execute();
-                $stmt->fetch();
-                //FIXME no errors caught, a print here does not work. But cannot create accounts
-            }
         ?>
+
     <body>
         <?php
             include_once 'includes/header.php';
@@ -46,7 +17,7 @@
                 <hr class="my-4">
                 <?php if(isUserConnected() == false){ ?>
                     <div class="form-container">
-                            <form class="form-horizontal" method = "POST" action = "inscription.php">
+                            <form class="form-horizontal" method ="POST" action ="traitementInsciption.php">
 
                                 <?php if (isset($error)) { ?>
                                     <div class="alert alert-danger">
@@ -56,15 +27,15 @@
 
                                 <div class="form-group" >
                                     <i class="fas input-icon"></i>
-                                    <input class="form-control" name="Nom" type="text" placeholder="Nom">
+                                    <input class="form-control" name="Nom" type="text" placeholder="Nom" required> 
                                 </div>
                                 <div class="form-group">
                                     <i class="fas input-icon"></i>
-                                    <input class="form-control" name="Prenom" type="text" placeholder="Prenom">
+                                    <input class="form-control" name="Prenom" type="text" placeholder="Prenom" required> 
                                 </div>
                                 <div class="form-group">
                                     <i class="fas input-icon"></i>
-                                    <input class="form-control" name="Promo" type="text" placeholder="Année de sortie">
+                                    <input class="form-control" name="Promo" type="text" placeholder="Année de sortie" required>
                                 </div>
                                 <div class="form-group">
                                     <i class="fas input-icon"></i>
@@ -72,11 +43,11 @@
                                 </div>
                                 <div class="form-group">
                                     <i class="fas input-icon"></i>
-                                    <input class="form-control" name="Mail" type="mail" placeholder="Mail">
+                                    <input class="form-control" name="Mail" type="mail" placeholder="Mail" required>
                                 </div>
                                 <div class="form-group">
                                     <i class="fas input-icon"></i>
-                                    <input class="form-control" name="Mot de passe" type="password" placeholder="Mot de passe">
+                                    <input class="form-control" name="motdepasse" type="password" placeholder="Mot de passe" required>
                                 </div>
                                 <div class="form-group">
                                     <i class="fas input-icon"></i>
@@ -85,14 +56,12 @@
 
                                 <div class="form-group" >
                                     <i class="fas input-icon"></i>
-                                    <input type="radio" name="genre" value="Homme"> Homme 
-                                    <input type="radio" name="genre" value="Femme"> Femme 
-                                    <input type="radio" name="genre" value="NB"> NB   
+                                    <input type="radio" name="genre" value="H" required> Homme 
+                                    <input type="radio" name="genre" value="F"> Femme 
+                                    <input type="radio" name="genre" value="NB" checked> NB   
                                 </div>
                                 
-
-
-                                <button class="btn signin">Allons-y !</button>
+                                <button type="submit" class="btn signin">Allons-y !</button>
                                 
                                 <span class="forgot-pass">Vous recevrez une confirmation de notre administrateur. Patience d'ici là ;)</span>
                             </form>
---
+++ traitementInsciption.php
@@ -0,0 +1,41 @@
+<html>
+        <?php
+            include_once 'includes/functions.php';
+            session_start();
+        
+            
+            //formulaire et création du compte. L'administrateur changera simplement le bool "validé"; 
+            //pour l'instant on peut créer un compte avec juste Nom, Promo, Mdp, pas besoin de if, le required html le fait pour nous
+            if($_POST != null)
+            {
+                $bdd = getLocalDb();
+                //trouver l'id du nouveau compte
+                $query= $bdd->prepare('select max(IdAlumni) from Alumni');
+                $query->execute();
+                $new_id = $query->fetch();
+                $new_id = $new_id['max(IdAlumni)'];
+                $new_id += 1;
+                //trouver l'id de la commune, pas accessible à l'élève qui s'inscrit
+                //fait changer le format. Demander le code postal par exemple
+                //$query= $bdd->prepare('select IdCommune from Commune where 'CodePostal = escape($_POST['AdressePostale'])');
+                //$idcommune=$query->fetch();
+                
+                //FIXME le 42 à la place d'idcommune dans la requete
+                $stmt = $bdd->prepare('insert into Alumni (IdAlumni, NomEleve, PrenomEleve, Promo, AdressePostale, Mail, Mdp, Genre, Tel, Valide, IdCommune, IdGestionnaire) values (:IdAlumni, :NomEleve, :PrenomEleve, :Promo, :AdressePostale, :Mail, :Mdp, :Genre, :Tel, 0, 1, 1)');
+                $stmt = $bdd->prepare('insert into Alumni (IdAlumni, NomEleve, PrenomEleve, Promo, AdressePostale, Mail, Mdp, Genre, Tel, Valide, IdCommune, IdGestionnaire) values (:IdAlumni, :NomEleve, :PrenomEleve, :Promo, :AdressePostale, :Mail, :Mdp, :Genre, :Tel, 0, 1, 1)');
+                $stmt->bindValue(':IdAlumni', $new_id);
+                $stmt->bindValue(':NomEleve', escape($_POST['Nom']));
+                $stmt->bindValue(':PrenomEleve', escape($_POST['Prenom']));
+                $stmt->bindValue(':Promo', escape($_POST['Promo']));
+                $stmt->bindValue(':AdressePostale', escape($_POST['Adresse']));
+                $stmt->bindValue(':Mail', escape($_POST['Mail']));
+                $stmt->bindValue(':Mdp', escape($_POST['motdepasse']));
+                $stmt->bindValue(':Genre', escape($_POST['genre']));
+                $stmt->bindValue(':Tel', escape($_POST['Tel']));
+                $stmt->execute();
+                $stmt->fetch();
+                
+            }
+            redirect('index.php');
+            
+        ?>
