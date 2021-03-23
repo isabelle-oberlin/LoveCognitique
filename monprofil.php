ba5db01a81985485b44c9c2073fe1acbb8dc98e2
PabloGri <pgribal@ensc.fr>
2021-03-23 18:42:15 +0100

    Update monprofil.php

--- monprofil.php
+++ monprofil.php
@@ -12,6 +12,15 @@
                 $requete->bindValue(':mailA', "{$mailAlumni}");
                 $requete->execute();
                 $eleve = $requete->fetch();
+                
+                $com = getLocalDb()->prepare('select * from commune where IdCommune=?');
+                $com->execute(array($eleve['IdCommune']));
+                $commune = $com->fetch();
+
+                $exp = getLocalDb()->prepare('select *, DATE_FORMAT(DateDeb, "%d/%m/%Y") as DateDebFr, DATE_FORMAT(DateFin, "%d/%m/%Y") as DateFinFr 
+                from experiences, organisations where IdAlumni=? and organisations.IdOrga = experiences.IdOrga order by DateFin desc');
+                $exp->execute(array($eleve['IdAlumni']));
+                $experiences = $exp->fetchAll();
             }
           
         ?>
@@ -22,28 +31,45 @@
         
             <?php if (!isAdminConnected())
             {?>
+        <br>
             <div class=" content">
             <div class="Jumbotron container">
-                <br>
                 <h3 class="display-4"><?= $eleve['PrenomEleve']," ",$eleve['NomEleve'] ?></h3>
-                <p class="lead">Promo: <?= $eleve['Promo']?></p>
-                <p class="lead">Adresse: <?= $eleve['AdressePostale']?></p>
-                <p class="lead">Téléphone: <?= $eleve['Tel']?></p>
-                <p class="lead">Genre: <?= $eleve['Genre']?></p>
+                <hr class="my-4">
+                
+                Promotion : <?= $eleve['Promo'] ?><br>
+                Commune : <?= $commune['NomCommune'], ", ", $commune['Pays'] ?><br>
+                Mail : <?= $eleve['Mail'] ?><br>
+                Téléphone : +33<?= $eleve['Tel'] ?><br>
+                Genre : <?= $eleve['Genre'] ?>
+                        
                 <hr class="my-2">
                 <p>Vous pouvez laisser votre numéro de téléphone ou votre mail en public, pour que les nouvelles générations vous demandent des conseils ;) Faites-leur profiter de votre expérience !</p>
+                <hr class="my-2">
+                <?php foreach($experiences as $experience){ ?>
+                    <div class="exp">
+                        <span class="bold">
+                        <?= $experience['TypeExp']," -" ?>
+                        <?= $experience['NomOrga'] ?><br></span>
+                        <?= "Du ",$experience['DateDebFr']," au ",$experience['DateFinFr'] ?> 
+                        <br><br>
+                        <?= $experience['Description'] ?>
+                    </div>
+                    <hr class="my-4">              
+                <?php } ?>
                 </div> 
                 </div>
-            </div>
+                
+            
 
-            <?php } else {?>
-            <br>
-            <div class=" content">
-                <div class="Jumbotron container">
-                <h3 class="display-4">Vous êtes administrateur, vous n'avez pas de profil particulier sur le site :)</h3>
-                </div>
-                </div>
-            <?php } ?>
+                <?php } else {?>
+                <br>
+                <div class=" content">
+                    <div class="Jumbotron container">
+                    <h3 class="display-4">Vous êtes administrateur, vous n'avez pas de profil particulier sur le site :)</h3>
+                    </div>
+                    </div>
+                <?php } ?>
         
         <?php
             include_once 'includes/footer.php';
