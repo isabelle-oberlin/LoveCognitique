d9cc600e901a8745aa2e6563a2d845d190fae670
Isabelle OBERLIN <isabelle.oberlin@epita.fr>
2021-03-24 14:41:17 +0100

    conflicts

--- indexadmin.php
+++ indexadmin.php
@@ -15,8 +15,7 @@
       <p class="lead">Accès à la liste de comptes en attente. Rendez-les fonctionnels pour que les élèves puissent se connecter!</p>
       <h1 class="display-5"><a href = "inscription.php">Créer un compte élève</a></h1>
       <p class="lead">Créer un compte à un élève. Vous pourrez ensuite lui transmettre les informations de connexion.</p>
-      <h1 class="display-3"><a href = "inscriptiongroupe.php">Créer un compte à un groupes d'élèves à partir d'un fichier</a></h1>
-      <p class="lead"> Créer de façon automatisée les comptes d'un groupe d'élève à partir d'une liste.</p>
+      <h1 class="display-3"><a href = "inscriptiongroupe.php">Créer un compte à un groupes d'élèves à partir d'un fichier</a></h1>      <p class="lead"> Créer de façon automatisée les comptes d'un groupe d'élève à partir d'une liste.</p>
       <h1 class="display-5"><a href = "adminstat.php">Statistiques</a></h1>
       <p class="lead">Accès aux statistiques sur les comptes des élèves et l'évolution de ce site.</p>
       <h1 class="display-5"><a href="promotions.php"> Consultation des comptes</a></h1>
