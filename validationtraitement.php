14ee4e3c20761cc0597d8353f2470fa69093f0b0
Isabelle OBERLIN <isabelle.oberlin@epita.fr>
2021-03-27 15:58:46 +0100

    validationtraitement

--- validationtraitement.php
+++ validationtraitement.php
@@ -5,8 +5,8 @@ require_once "includes/functions.php";
     if (isset($_POST['id']))
     {
         $bdd = getLocalDb();
-        $query= $bdd->prepare('update alumni set Valide=1 WHERE Alumni.IdAlumni = :idalumni ');
-        $query->bindValue(':idalumni', "{$_POST['id']}%");
+        $query= $bdd->prepare('update alumni set Valide=1 WHERE alumni.IdAlumni = :idalumni ');
+        $query->bindValue(':idalumni', "{$_POST['id']}");
         $query->execute();
     }
     
